57f9da0d1be14192042c754df38569fdcc1b71e8
Ali-Tigui <75787851+Ali-Tigui@users.noreply.github.com>
2021-04-20 00:50:55 +0100

    Add files via upload

---
+++ javascript/js-DisableRightClick-main/main.js
@@ -0,0 +1,12 @@
+//document.oncontextmenu=function(e){
+//    e.preventDefault();
+//}
+// or we can do it using add eventlistenet
+document.addEventListener('contextmenu',function(e){
+    e.preventDefault()
+});
+// preventDefault
+/*
+it the way to stop ths default of event 
+e is the event 
+*/
