27754c63c1dc41e1d0d03bfcd426fba03a3c1e32
Ali-Tigui <75787851+Ali-Tigui@users.noreply.github.com>
2021-04-20 01:10:20 +0100

    Delete javascript/js-Calculetextarea-main directory

--- javascript/js-Calculetextarea-main/main.js
+++
@@ -1,8 +0,0 @@
-const el=document.querySelector('span'),
-      el2=document.querySelector("textarea");
-el2.oninput=function(){
- var m=el2.getAttribute('max-length')-this.value.length;
-    el.textContent="it still " +m;
- if(m<=0){
- el.setAttribute("class","dngr")
- }else{el.setAttribute("class","")}}
