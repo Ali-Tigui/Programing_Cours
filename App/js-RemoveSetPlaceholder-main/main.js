b504e9aaded270538e2eb4ca022c6522b5407ee2
Ali-Tigui <75787851+Ali-Tigui@users.noreply.github.com>
2021-04-20 01:08:20 +0100

    Delete App directory

--- App/js-RemoveSetPlaceholder-main/main.js
+++
@@ -1,6 +0,0 @@
-const inp=document.getElementsByTagName('input');
-for(i of inp){
-    i.setAttribute("data",i.getAttribute("placeholder"));
-    i.onblur=function(){this.setAttribute("placeholder",this.getAttribute("data"))};
-i.onfocus=function(){this.removeAttribute("placeholder")};
-              }
--- App/js-WriteInElement-main/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('input').onkeypress=function(){
-document.querySelector('h1').textContent=this.value;
-}
-document.querySelector('textarea').onkeypress=function(){
-document.querySelector('p').textContent=this.value;
-}
